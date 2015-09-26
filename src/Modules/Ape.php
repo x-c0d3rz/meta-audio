d30e109c5edee5fd5dcb490659651851fb57ef87
Craig Duncan <git@duncanc.co.uk>
2015-09-26 22:44:39 +0100

    Add write support to the ape module

--- src/Modules/Ape.php
+++ src/Modules/Ape.php
@@ -9,6 +9,7 @@ use duncan3dc\MetaAudio\Exception;
  */
 class Ape extends AbstractModule
 {
+    const PREAMBLE = "APETAGEX";
 
     /**
      * Get all the tags from the currently loaded file.
@@ -19,8 +20,9 @@ class Ape extends AbstractModule
     {
         $this->file->fseek(0, \SEEK_END);
 
-        $position = $this->file->getPreviousPosition("APETAGEX");
+        $position = $this->file->getPreviousPosition(self::PREAMBLE);
 
+        # If there is no APE tag then just return an empty array
         if ($position === false) {
             return [];
         }
@@ -51,8 +53,8 @@ class Ape extends AbstractModule
     protected function parseHeader()
     {
         $preamble = $this->file->fread(8);
-        if ($preamble !== "APETAGEX") {
-            throw new Exception("Invalid Ape tag, expected [APETAGEX], got [{$preamble}]");
+        if ($preamble !== self::PREAMBLE) {
+            throw new Exception("Invalid Ape tag, expected [" . self::PREAMBLE . "], got [{$preamble}]");
         }
 
         $version = unpack("L", $this->file->fread(4))[1];
@@ -65,7 +67,7 @@ class Ape extends AbstractModule
             "size"      =>  $size,
             "items"     =>  $items,
             "flags"     =>  $flags,
-            "footer"    =>  !($flags & 0x20000000),
+            "footer"    =>  !($flags & 0x20),
         ];
 
         # Skip the empty space at the end of the header
@@ -102,6 +104,84 @@ class Ape extends AbstractModule
         }
 
         return [$key, $value];
+    }
+
+
+    /**
+     * Write the specified tags to the currently loaded file.
+     *
+     * @param array The tags to write as key/value pairs
+     *
+     * @return void
+     */
+    protected function putTags(array $tags)
+    {
+        # Get the contents of the file (without the ape tags)
+        $contents = "";
+        $this->file->rewind();
+        while (true) {
+            $position = $this->file->getNextPosition(self::PREAMBLE);
+            if ($position === false) {
+                break;
+            }
+
+            # Get any content before the ape tag
+            if ($position > 0) {
+                $contents .= $this->file->fread($position);
+            }
+
+            $header = $this->parseHeader();
+            $this->file->fseek($header["size"], \SEEK_CUR);
+        }
+
+        # Read the rest of the file (following the last ape tag)
+        $contents .= $this->file->readAll();
+
+        # Generate the new ape tags
+        $tags = $this->createTagData();
+
+        # Empty the file and position at the start so we can overwrite
+        $this->file->ftruncate(0);
+        $this->file->rewind();
+
+        $this->file->fwrite($contents);
+        $this->file->fwrite($tags);
+    }
+
+
+    /**
+     * Create the header for the file.
+     *
+     * @return string
+     */
+    protected function createTagData()
+    {
+        $tags = "";
+        foreach ($this->tags as $key => $value) {
+            $tags .= pack("L", strlen($value));
+            $tags .= pack("L", 0);
+            $tags .= $key;
+            $tags .= pack("c", 0x00);
+            $tags .= $value;
+        }
+
+        $footer = self::PREAMBLE;
+
+        # Version
+        $footer .= pack("L", 2000);
+
+        # Size (including the bytes for the footer)
+        $footer .= pack("L", strlen($tags) + 32);
+
+        # Number of tags
+        $footer .= pack("L", count($this->tags));
+
+        # Flags
+        $footer .= pack("L", 0);
+
+        $footer .= str_repeat(" ", 8);
+
+        return $tags . $footer;
     }
 
 
@@ -157,5 +237,70 @@ class Ape extends AbstractModule
     public function getYear()
     {
         return (int) $this->getTag("year");
+    }
+
+
+    /**
+     * Set the track title.
+     *
+     * @param string $title The title name
+     *
+     * @return void
+     */
+    public function setTitle($title)
+    {
+        return $this->setTag("title", $title);
+    }
+
+
+    /**
+     * Set the track number.
+     *
+     * @param int $track The track number
+     *
+     * @return void
+     */
+    public function setTrackNumber($track)
+    {
+        return $this->setTag("tracknumber", $track);
+    }
+
+
+    /**
+     * Set the artist name.
+     *
+     * @param string $artist The artist name
+     *
+     * @return void
+     */
+    public function setArtist($artist)
+    {
+        return $this->setTag("artist", $artist);
+    }
+
+
+    /**
+     * Set the album name.
+     *
+     * @param string $album The album name
+     *
+     * @return void
+     */
+    public function setAlbum($album)
+    {
+        return $this->setTag("album", $album);
+    }
+
+
+    /**
+     * Set the release year.
+     *
+     * @param int $year The release year
+     *
+     * @return void
+     */
+    public function setYear($year)
+    {
+        return $this->setTag("year", $year);
     }
 }
